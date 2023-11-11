e066527a02c28183d5f414764a11c1f2b32b88b1
Jens Heine <binbash@gmx.net>
2023-11-11 12:57:57 +0100

    Commandline parser

--- scanwifi.sh
+++ scanwifi.sh
@@ -12,8 +12,13 @@ AIRODUMP_PID=-1
 KISMETCSV2SQLITE_PID=-1
 BASE_DIR="./"
 KISMETCSV2SQLITE_BIN="kismetcsv2sqlite.sh"
+WLAN_DEV="wlan0"
+DATABASE_INSERT_INTERVAL_SEC=1
 
 
+#
+# Functions
+#
 function ctrl_c() {
 	set +e
 	echo "Stopping..."
@@ -25,13 +30,51 @@ function ctrl_c() {
 	exit 0
 }
 
+print_help() {
+  echo "2023 Jens Heine"
+  echo "Scan wifi's and collect the data together with gps data in a sqlite db"
+  echo "Usage: `basename $0` [-d DATABASE] [-s SOURCE_DIRECTORY]|[-c CSV_FILE] [-i REPEAT_INTERVAL [-t WIRELESS_TABLE_NAME]"
+  echo " -B BASE_DIR"
+  echo "    Base directory where the airodump files and the sqlite db will be saved."
+  echo " -w WLAN_DEV"
+  echo "    Set the wlan interface. Example: -w wlan0"
+  echo " -i DATABASE_INSERT_INTERVAL_SEC"
+  echo "    Insert every x seconds all data collected into the database."
+}
 
 
+#
+# Parse command line args
+#
+while getopts 'B:w:i:?' opt; do
+  case "$opt" in
+    B)
+      arg="$OPTARG"
+      BASE_DIR="${OPTARG}"
+      ;;
+    w)
+      arg="$OPTARG"
+      WLAN_DEV="${OPTARG}"
+      ;;
+    i)
+      arg="$OPTARG"
+      DATABASE_INSERT_INTERVAL_SEC="${OPTARG}"
+      ;;
+    ?|h)
+      print_help
+      exit 0
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+#
+# Main
+#
 cd "$BASE_DIR"
-#airodump-ng -w outfile --manufacturer --uptime --wps wlan1 --gpsd
 
 # Run airmon in background
-airodump-ng -w "$SCANFILE_PREFIX" wlan1 --gpsd -K 1 &
+airodump-ng -w "$SCANFILE_PREFIX" $WLAN_DEV --gpsd -K 1 &
 
 # Remember airodump PID
 AIRODUMP_PID=$!
